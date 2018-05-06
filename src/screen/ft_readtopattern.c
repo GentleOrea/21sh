f8417655ae7924a6c1786a15dda1fb050e1c7321
tcharrie <bial@protonmail.ch>
2018-05-06 22:05:34 +0200

    ¶T: ft_getpos réécrite

--- src/screen/ft_readtopattern.c
+++ src/screen/ft_readtopattern.c
@@ -19,13 +19,26 @@ char	*ft_addmemory(char **str, int size, int add)
 	return (val);
 }
 
-char	ft_readtostr(char *str, int fd, int try)
+char	*ft_readtostr_a(char *buff, int fd, int over)
+{
+	char	*tmp;
+
+	tmp = over >= 0 ? ft_strdupto(&buff[over], ';') : 0;
+	if (over >= 0)
+		ft_addtofd(buff[over + 1 + ft_strlento(&buff[over], '\n')], fd);
+	if (over >= 0)
+		buff[over] = 0;
+	ft_addtofd(buff, fd);
+	free(buff);
+	return (tmp);
+}
+
+char	*ft_readtostr(char *str, int fd, int try)
 {
 	char	*buff;
 	int		re;
 	int		over;
 	int		size;
-	char	*tmp;
 
 	over = -1;
 	re = 0;
@@ -34,22 +47,13 @@ char	ft_readtostr(char *str, int fd, int try)
 		return (-1);
 	while (try-- > 0 && re < 5 && over == -1)
 	{
-		re += read(STDIN_FILENO, &buff[re], size - re);
-		over = ft_strinstr(buff, str);
-		if (over == -1 && re == size)
+		re += read(fd, &buff[re], size - re);
+		if ((over = ft_strinstr(buff, str)) == -1 && re == size)
 		{
 			if (!ft_addmemory(&buff, size, 100))
 				return (-1);
 			size += 100;
 		}
 	}
-	tmp = over >= 0 ? ft_strdup(&buff[over]) : 0;
-	if (over >= 0)
-		ft_addtofd(buff[over + 1 + ft_strlento(&buff[over], '\n')],
-			STDIN_FILENO);
-	if (over >= 0)
-		buff[over] = 0;
-	ft_addtofd(buff, STDIN_FILENO);
-	free(buff);
-	return (tmp);
+	return (ft_readtostr_a(buff, fd, over));
 }
--- tcharrie/include.h
+++
@@ -1,9 +0,0 @@
-#ifndef MINISHELL_H
-# define MINISHELL_H
-# define ERR -1
-//# include <curses.h>
-# include <term.h>
-# define BUFFSIZE 30
-
-typedef struct stat	t_stat;
-#endif
