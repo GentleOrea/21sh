a8e0493752a031ae123172bbc3fe972ce68d36fd
Thibaud CHARRIER <tcharrie@e2r8p10.42.fr>
2018-05-08 12:29:33 +0200

    ∆

--- src/initialisation/ft_terminal.c
+++ src/initialisation/ft_terminal.c
@@ -6,7 +6,7 @@
 /*   By: tcharrie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/28 09:52:26 by tcharrie          #+#    #+#             */
-/*   Updated: 2018/05/08 12:06:46 by tcharrie         ###   ########.fr       */
+/*   Updated: 2018/05/08 12:29:18 by tcharrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,13 +41,13 @@ int		ft_setattr(void)
 {
 	t_termios	term;
 
-	if (tcgetattr(0, &term) == -1)
+	if (tcgetattr(STDIN_FILENO, &term) == -1)
 		ft_fatal("Can't recover the terminal structure");
 	ft_terminal_reset(&term);
 	term.c_lflag &= ~(ICANON | ECHO);
 	term.c_cc[VTIME] = 0;
 	term.c_cc[VMIN] = 1;
-	if (tcsetattr(0, 0, &term) == -1)
+	if (tcsetattr(STDIN_FILENO, 0, &term) == -1)
 		ft_fatal("Can't set the terminal to have great power, exit ...");
 	return (1);
 }
@@ -59,6 +59,6 @@ int		ft_terminal_reset(t_termios *term)
 	if (term)
 		save = *term;
 	else
-		return (tcsetattr(0, 0, &save));
+		return (tcsetattr(STDIN_FILENO, 0, &save));
 	return (1);
 }
