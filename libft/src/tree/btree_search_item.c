52eca0feae829bbb62db32c69737526fd16c641d
Thibaud CHARRIER <tcharrie@e3r5p14.42.fr>
2018-05-17 11:40:10 +0200

    ∆Tree unique

--- libft/src/tree/btree_search_item.c
+++ libft/src/tree/btree_search_item.c
@@ -6,7 +6,7 @@
 /*   By: tcharrie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/25 12:51:38 by tcharrie          #+#    #+#             */
-/*   Updated: 2018/05/08 12:57:28 by tcharrie         ###   ########.fr       */
+/*   Updated: 2018/05/17 11:39:58 by tcharrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,15 @@
 void	*btree_search_item(t_btree *root, void *data_ref,
 		int (*cmpf)(void *, void *))
 {
-	void	*found;
+	int		val;
 
-	found = NULL;
-	if (root->left)
-		found = btree_search_item(root->left, data_ref, cmpf);
-	if (!found && !(cmpf(data_ref, root->item)))
-		found = root->item;
-	if (!found && root->right)
-		found = btree_search_item(root->right, data_ref, cmpf);
-	return (found);
+	if (!root)
+		return (0);
+	val = cmpf(data_ref, root->item);
+	if (!val)
+		return (root->item);
+	else if (val > 0)
+		return (btree_search_item(root->right, data_ref, cmpf));
+	else
+		return (btree_search_item(root->left, data_ref, cmpf));
 }
