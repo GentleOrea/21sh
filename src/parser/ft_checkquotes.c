68cb13b4b2b74e7a45bbf880629df9fb14623ba8
tcharrie <bial@protonmail.ch>
2018-05-24 18:45:24 +0200

    Un peu de parser & de comm & un ft_getenv depuis le fichier

--- src/parser/ft_checkquotes.c
+++ src/parser/ft_checkquotes.c
@@ -12,7 +12,11 @@ int	ft_checkquotes(char *str)
 	while (str[i])
 	{
 		if (sep[2] && str[i] == '`' && sep[0] && !sep[1])
+		{
+			ft_printf("Parse error : unexpected EOF while looking for\
+			matching `%c%c\n", sep[0], sep[0]);
 			return (0);
+		}
 		ft_separator(str[i], sep, &sep[1], &sep[2]);
 		i += ft_lenchar_r(str, i);
 	}
