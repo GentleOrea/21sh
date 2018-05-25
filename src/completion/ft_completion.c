ba89540168f66074497afc1d9176dd41bcf6288a
Thibaud CHARRIER <tcharrie@e1r2p19.42.fr>
2018-05-25 14:55:52 +0200

    Ahead of its own time

--- src/completion/ft_completion.c
+++ src/completion/ft_completion.c
@@ -6,7 +6,7 @@
 /*   By: tcharrie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/11 12:56:06 by tcharrie          #+#    #+#             */
-/*   Updated: 2018/05/25 13:33:40 by tcharrie         ###   ########.fr       */
+/*   Updated: 2018/05/25 14:54:47 by tcharrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ static int	ft_completion_(t_line *line, int *val, char *left, int type)
 	int		re;
 	int		bq;
 
-	ft_errorlog(type == COMPLETION_FILE ? "FILE" : type == COMPLETION_COM ? "COMMANDE" : 0);
 	ft_completion_count(CODE_PUSH);
 	ft_completion_savetype(CODE_SET, type);
 	if (val[4])
@@ -32,10 +31,11 @@ static int	ft_completion_(t_line *line, int *val, char *left, int type)
 	right = (type == COMPLETION_FILE) ? (ft_completion_getfilename(left,
 		ft_completion_count(CODE_GET), bl, sep)) : ft_completion_com(left,
 		ft_completion_count(CODE_GET), bl, sep);
+	ft_completion_saveleft(CODE_SET, ft_strdup(left));
+	ft_strdel(&left);
 	if (!right)
 		return (ft_completion_reset() ? -1 : -1);
 	re = (ft_printstr(line, right, val) < 0) ? (-1) : (0);
-	ft_completion_saveleft(CODE_SET, left);
 	ft_completion_lastwrite(CODE_SET, re ? 0 : ft_strlen(right));
 	ft_strdel(&right);
 	return (re);
@@ -52,9 +52,9 @@ int		ft_completion(t_line *line, int *val)
 		if (ft_nerase(line, val, ft_completion_lastwrite(CODE_GET, 0)) < 0)
 			return (-1);
 		type = ft_completion_savetype(CODE_GET, 0);
-		leftpurged = (type > 0 ? ft_completion_saveleft(CODE_GET, 0) : 0);
+		leftpurged = (type > 0 ?
+				ft_strdup(ft_completion_saveleft(CODE_GET, 0)) : 0);
 		ft_errorlog("prev");
-		ft_errorlog(leftpurged);
 	}
 	else
 	{
@@ -64,7 +64,7 @@ int		ft_completion(t_line *line, int *val)
 		ft_strdel(&left);
 		ft_errorlog("new");
 	}
-	if (!type || !leftpurged)
+	if (type <= 0 || !leftpurged)
 		return (ft_completion_reset() ? -1 : -1);
 	return (ft_completion_(line, val, leftpurged, ft_completion_type(line, val)));
 }
