856aaf8f2117a6ef69f748dd80671a93dbadbcf6
Thibaud CHARRIER <tcharrie@e3r5p19.42.fr>
2018-06-02 15:25:18 +0200

    Variables

--- src/variables/ft_unset.c
+++ src/variables/ft_unset.c
@@ -6,7 +6,7 @@
 /*   By: tcharrie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/02 13:17:26 by tcharrie          #+#    #+#             */
-/*   Updated: 2018/06/02 13:36:11 by tcharrie         ###   ########.fr       */
+/*   Updated: 2018/06/02 15:23:10 by tcharrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,27 @@
 
 void	ft_unset(char **arg, char ***env)
 {
+	t_btree		**root;
+	t_variable	cmp;
+	t_variable	*tmp;
+	char		**unsetenv;
 
+	if (!arg || !env || !*env || !*arg)
+		return ;
+	if (!(root = ft_variable(0)))
+		return ;
+	ft_bzero((void*)&cmp, sizeof(t_variable));
+	if (!(cmp.name = arg[1]))
+	{
+		ft_putstr_fd("21sh : unset VARNAME\n", 2);
+		return ;
+	}
+	tmp = btree_search_item(*root, (void*)&cmp, &ft_variablecmp);
+	if (tmp && tmp->deported)
+	{
+		unsetenv = (char*[3]){"unsetenv", arg[1], 0};
+		ft_unsetenv(unsetenv, env);
+	}
+	btree_remove_data(root, (void*)&cmp, ft_variablecmp, ft_variabledel);
 
 }
